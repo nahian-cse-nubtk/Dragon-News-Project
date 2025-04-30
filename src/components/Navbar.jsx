d085b2d69f75d2d8a56aa15cd5d6daf4a580149f
ferdouszihad <f.zihad333@gmail.com>
2025-05-01 01:26:42 +0600

    logOut+ Login implementation done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,17 @@ import { Link, NavLink } from "react-router";
 import userIcon from "../assets/user.png";
 import { AuthContext } from "../provider/AuthProvider";
 const Navbar = () => {
-  const { user } = use(AuthContext);
+  const { user, logOut } = use(AuthContext);
+  const handleLogOut = () => {
+    console.log("user trying to LogOut");
+    logOut()
+      .then(() => {
+        alert("You Logged Out successfully");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <div className="flex justify-between items-center">
       <div className="">{user && user.email}</div>
@@ -14,9 +24,15 @@ const Navbar = () => {
       </div>
       <div className="login-btn flex gap-5">
         <img src={userIcon} alt="" />
-        <Link to="/auth/login" className="btn btn-primary px-10 ">
-          Login
-        </Link>
+        {user ? (
+          <button onClick={handleLogOut} className="btn btn-primary px-10 ">
+            LogOut
+          </button>
+        ) : (
+          <Link to="/auth/login" className="btn btn-primary px-10 ">
+            Login
+          </Link>
+        )}
       </div>
     </div>
   );
